74c912f02d48db82ebd307f43a7ac2482138b86b
John Jairo Restrepo Montoya <johnjairo.restrepo@globant.com>
2022-03-09 17:43:20 -0500

    other folder

---
+++ topic6Bill/src/main/java/com/challenge/topic6Bill/Topic6BillApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.topic6Bill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Topic6BillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Topic6BillApplication.class, args);
+	}
+
+}
---
+++ topic6Bill/src/test/java/com/challenge/topic6Bill/Topic6BillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.topic6Bill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Topic6BillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
